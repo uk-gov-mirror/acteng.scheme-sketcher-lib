d8f767fec8aedafad9c5df795c97adc45427570e
Peter York <peter.york@activetravelengland.gov.uk>
2026-04-21 15:18:13 +0100

    export routemode for route check tool

--- src/lib/draw/route/index.ts
+++ src/lib/draw/route/index.ts
@@ -1,2 +1,3 @@
 export { default as RouteControls } from "./RouteControls.svelte";
 export { default as RouteSnapperLoader } from "./RouteSnapperLoader.svelte";
+export { default as RouteMode } from "./RouteMode.svelte";
